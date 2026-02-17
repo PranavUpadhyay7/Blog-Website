a3e53e8eb7ef28d248e9073806d4560f7f647d60
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2026-02-17 17:24:47 +0530

    Initial clean commit with proper gitignore

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Blog = async () => {
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Blog;
