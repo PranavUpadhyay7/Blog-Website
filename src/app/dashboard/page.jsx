18aadf4de5422cea3eea7af0c6f566a57ab73b25
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2026-03-18 17:09:12 +0530

    Dashboard Changes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.css";
 
 const Dashboard = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return res.json();
+    };
+  }, []);
+
+  return <div className={styles.container}></div>;
 };
 
 export default Dashboard;
