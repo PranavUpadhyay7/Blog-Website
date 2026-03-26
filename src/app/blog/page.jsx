fac68f623da5599579e2f85757057ddaf754e6ab
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2026-03-26 13:46:12 +0530

    Map function in blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,39 +3,41 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
+async function getData() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return res.json();
+}
+
 const Blog = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/contact.png"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>desc</p>
-        </div>
-      </Link>
-      <Link href="/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/contact.png"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link key={item.id} href="/testId" className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/contact.png"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>desc</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
