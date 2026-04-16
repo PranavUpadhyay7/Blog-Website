cd4bd95c6383450350477ae3ead72771e226ad0f
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2026-04-16 21:29:46 +0530

    nothing changed

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./page.module.css";
 
 const Register = () => {
-  return <div>Register.</div>;
+  return <div>Register</div>;
 };
 
 export default Register;
